3a38c3c6e3a408fe64061633874d77043f37da56
Aydrian J. Howard <aydrian@gmail.com>
2017-07-19 16:25:02 -0400

    Initial react app with firebase connection

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    };
+  }
+
+  componentDidMount() {
+    const ref = firebase.database().ref();
+    const playersRef = ref.child('players');
+
+    console.log(playersRef.key);
+    playersRef.on('value', (snap) => {
+      let newPlayers = [];
+      snap.forEach(childSnapshot => {
+        newPlayers.push(childSnapshot.val());
+      })
+      console.log(newPlayers);
+      this.setState({
+        players: newPlayers
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,6 +36,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <h1>Players: {this.state.players.length}</h1>
+        <ul className="list-group">
+          {this.state.players.map((player, index) => (
+            <li className="list-group-item" key={index}>
+              A {player.animal.name} named {player.subject}. {player.animal.emoji}
+            </li>
+          ))}
+        </ul>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyBD6zLRah7dnPRL2FpKLLy7gGH6SIrl37c",
+  authDomain: "sp-firebase-app.firebaseapp.com",
+  databaseURL: "https://sp-firebase-app.firebaseio.com",
+  projectId: "sp-firebase-app",
+  storageBucket: "sp-firebase-app.appspot.com",
+  messagingSenderId: "910951010182"
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
