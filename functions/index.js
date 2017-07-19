ea44e7396d5302efd756c67c828d4582effd06a5
Aydrian J. Howard <aydrian@gmail.com>
2017-07-19 14:55:50 -0400

    Updated incoming route with player info and random animal emoji

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,44 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+const animals = [
+  {name: 'dog', emoji: '🐶'},
+  {name: 'cat', emoji: '🐱'},
+  {name: 'mouse', emoji: '🐭'},
+  {name: 'hamster', emoji: '🐹'},
+  {name: 'bunny', emoji: '🐰'},
+  {name: 'bear', emoji: '🐻'},
+  {name: 'panda', emoji: '🐼'},
+  {name: 'koala', emoji: '🐨'},
+  {name: 'tiger', emoji: '🐯'},
+  {name: 'lion', emoji: '🦁'},
+  {name: 'cow', emoji: '🐮'},
+  {name: 'pig', emoji: '🐷'},
+  {name: 'frog', emoji: '🐸'},
+  {name: 'octopus', emoji: '🐙'},
+  {name: 'monkey', emoji: '🐵'},
+  {name: 'chicken', emoji: '🐔'},
+  {name: 'penguin', emoji: '🐧'},
+  {name: 'pigeon', emoji: '🐦'},
+  {name: 'wolf', emoji: '🐺'},
+  {name: 'boar', emoji: '🐗'},
+  {name: 'horse', emoji: '🐴'},
+  {name: 'unicorn', emoji: '🦄'}
+];
+
+const escapeEmailAddress = (email) => {
+  if (!email) return false
+
+  // Replace '.' (not allowed in a Firebase key) with ',' (not allowed in an email address)
+  email = email.toLowerCase();
+  email = email.replace(/\./g, ',');
+  return email;
+}
+
+const getRandomAnimal = () => {
+  return animals[Math.floor(Math.random() * (animals.length + 1))];
+}
+
 exports.incoming = functions.https.onRequest((req, res) => {
   if (req.method != "POST") {
      res.status(404).send();
@@ -14,7 +52,15 @@ exports.incoming = functions.https.onRequest((req, res) => {
   for (let i = 0; i < batch.length; i++) {
     // For this application, we can safely assume the batch will only
     // contain relay_message events
-    admin.database().ref('/raw-inbound').push(batch[i].msys.relay_message);
+    const msg = batch[i].msys.relay_message;
+    let player = {
+      email: msg.friendly_from || msg.msg_from,
+      to: msg.rcpt_to,
+      subject: msg.content.subject,
+      body: msg.content.text || msg.content.html,
+      animal: getRandomAnimal()
+    }
+    admin.database().ref(`/players/${escapeEmailAddress(player.email)}`).set(player);
   }
   res.status(200).send();
 });
