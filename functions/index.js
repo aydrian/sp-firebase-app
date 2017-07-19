e403253f6b9a7216d0fb0b6c2e195f9e8e26bf4e
Aydrian J. Howard <aydrian@gmail.com>
2017-07-19 10:10:31 -0400

    Created incoming cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.incoming = functions.https.onRequest((req, res) => {
+  if (req.method != "POST") {
+     res.status(404).send();
+     return;
+  }
+
+  const batch = req.body;
+  for (let i = 0; i < batch.length; i++) {
+    // For this application, we can safely assume the batch will only
+    // contain relay_message events
+    admin.database().ref('/raw-inbound').push(batch[i].msys.relay_message);
+  }
+  res.status(200).send();
+});
