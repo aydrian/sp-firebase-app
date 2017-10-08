547348fc4e5b960b330f1fd0df820a8b404ce879
Aydrian J. Howard <aydrian@gmail.com>
2017-10-08 09:18:54 -0700

    standardjs

--- src/components/Animals/Animal.js
+++ src/components/Animals/Animal.js
@@ -1,44 +1,44 @@
-import React from 'react';
-import './Animal.css';
-import moment from 'moment';
-import _ from 'lodash';
+import React from 'react'
+import './Animal.css'
+import moment from 'moment'
+import _ from 'lodash'
 
 const formatDate = (unixStr) => {
-  return moment(unixStr, "X").format("llll")
+  return moment(unixStr, 'X').format('llll')
 }
 
 const Opens = ({opens}) => {
   let arr = _.sortBy(_.toArray(opens), (item) => {
-    return moment(item.timestamp, "X");
-  }).reverse();
+    return moment(item.timestamp, 'X')
+  }).reverse()
   return (
-    <div className="Open"><span className="title">Last Opened:</span> <br />{formatDate(arr[0].timestamp)}</div>
-  );
-};
+    <div className='Open'><span className='title'>Last Opened:</span> <br />{formatDate(arr[0].timestamp)}</div>
+  )
+}
 
 const Clicks = ({clicks}) => {
-  let arr = _.toArray(clicks);
-  let groups = _.groupBy(arr, 'target_link_name');
-  let group_keys = Object.keys(groups);
+  let arr = _.toArray(clicks)
+  let groups = _.groupBy(arr, 'target_link_name')
+  let groupKeys = Object.keys(groups)
   return (
-    <div className="Click"><span className="title">Clicks:</span>
-      <ul className="Click-group">
-        {group_keys.map((group_key, index) => (
-          <li key={index}><a href={groups[group_key][0].target_link_url} >{group_key}</a>: {groups[group_key].length}</li>
+    <div className='Click'><span className='title'>Clicks:</span>
+      <ul className='Click-group'>
+        {groupKeys.map((groupKey, index) => (
+          <li key={index}><a href={groups[groupKey][0].target_link_url} >{groupKey}</a>: {groups[groupKey].length}</li>
         ))}
       </ul>
     </div>
-  );
+  )
 }
 
 const Animal = ({player}) => {
   return (
-    <li className="Animal-group-item">
-      <div className="Animal">
-        <div className="Animal-emoji">{player.animal.emoji}</div>
-        <div className="Animal-name">{player.subject}</div>
+    <li className='Animal-group-item'>
+      <div className='Animal'>
+        <div className='Animal-emoji'>{player.animal.emoji}</div>
+        <div className='Animal-name'>{player.subject}</div>
         {player.delivered &&
-          <div className="Delivered"><span className="title">Email Delivered:</span><br /> {formatDate(player.delivered)}.</div>
+          <div className='Delivered'><span className='title'>Email Delivered:</span><br /> {formatDate(player.delivered)}.</div>
         }
         {player.opens &&
           <Opens opens={player.opens} />
@@ -51,7 +51,7 @@ const Animal = ({player}) => {
         }
       </div>
     </li>
-  );
-};
+  )
+}
 
-export default Animal;
+export default Animal
--- src/components/Animals/Animals.js
+++ src/components/Animals/Animals.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Animal from './Animal';
-import './Animal.css';
+import React from 'react'
+import Animal from './Animal'
+import './Animal.css'
 
 const Animals = ({players}) => {
   return (
-    <ul className="Animal-group">
+    <ul className='Animal-group'>
       {players.map((player, index) => (
         <Animal player={player} key={index} />
       ))}
     </ul>
-  );
-};
+  )
+}
 
-export default Animals;
+export default Animals
